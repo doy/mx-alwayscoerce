538d9d4ec7ec0076a1c11ce291fb549564838c28
Rafael Kitover <rkitover@cpan.org>
2010-03-06 00:28:59 -0500

    remove t/00-load.t (was failing), release

--- lib/MooseX/AlwaysCoerce.pm
+++ lib/MooseX/AlwaysCoerce.pm
@@ -18,11 +18,11 @@ MooseX::AlwaysCoerce - Automatically enable coercions for Moose attributes
 
 =head1 VERSION
 
-Version 0.03
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
