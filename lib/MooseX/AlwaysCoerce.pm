9e804fe12e784d73f58f24ca322b5ae8405f3f5e
Rafael Kitover <rkitover@cpan.org>
2010-07-12 20:06:07 -0400

    release

--- lib/MooseX/AlwaysCoerce.pm
+++ lib/MooseX/AlwaysCoerce.pm
@@ -18,11 +18,11 @@ MooseX::AlwaysCoerce - Automatically enable coercions for Moose attributes
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 =head1 SYNOPSIS
 
